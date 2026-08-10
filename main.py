22cb83141e8f627c61583209281c40f188d49f5c
damirusasel-dotcom <damiru.sasel@gmail.com>
2026-08-10 11:59:23 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+def on_button_pressed_a():
+    basic.show_string("DAMIRU ")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.show_string("DIKSHAN")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_string("YASIRU")
+input.on_button_pressed(Button.B, on_button_pressed_b)
---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("DAMIRU ")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("DIKSHAN")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("YASIRU")
+})
